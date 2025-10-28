6ed99467adeb953796f541efc28ff6bd9844198f
Jamila Moraes Cardoso <jmc18.ads@gmail.com>
2025-10-28 10:36:48 -0300

    :tada: Commint inicial

---
+++ src/main/java/com/generation/inovarh/InovarhApplication.java
@@ -0,0 +1,13 @@
+package com.generation.inovarh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InovarhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InovarhApplication.class, args);
+	}
+
+}
